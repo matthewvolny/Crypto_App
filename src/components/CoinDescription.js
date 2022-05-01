088d20864e8f6c70d487a88eb8c8a643e235f6f7
matthewvolny <matthew.volny@gmail.com>
2022-05-01 17:49:23 -0400

    improved coin description styling

--- src/components/CoinDescription.js
+++ src/components/CoinDescription.js
@@ -7,10 +7,6 @@ export default function CoinDescription() {
 
   return (
     <div className="coin-description-container">
-      {/* <div className="section">
-        <div className="coin-description-heading">Name</div>
-        <div>{selectedCoinData?.name}</div>
-      </div> */}
       <div className="section">
         <div className="coin-description-heading">Rank</div>
         <div>#{selectedCoinData?.rank}</div>
